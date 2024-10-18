f496eab41949cd1dc33c80150fc8b03f4012d036
Harirajhari <hariraj121203@gmail.com>
2024-10-18 11:45:11 +0530

    forth-page need do

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -4,6 +4,7 @@ import productImage from '../../assests/images/info-img-1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { faShieldAlt, faBug, faCloud, faClipboard, faSyncAlt, faFileAlt, faClock, faServer, faCog, faKey, faCogs, faLock, faTerminal } from '@fortawesome/free-solid-svg-icons';
+import videoimage from "../../assests/images/videoimg.png"
 
 
 const Product = () => {
@@ -72,6 +73,15 @@ const Product = () => {
         setFlippedStates(newFlippedStates);
     };
 
+    const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+            const handlePlayVideo = () => {
+                setIsVideoPlaying(true);
+            };
+            const handleCloseVideo = () => {
+                setIsVideoPlaying(false);
+            };
+
     return (
         <>
             <div className="product-page">
@@ -128,7 +138,59 @@ const Product = () => {
 </section> */}
 
 
-                <section className="section second-section">
+<section className="section second-section">
+                    <h1>Differences Between Infoziant DAST Pro and Free Versions</h1>
+                            <div className="video-title">
+                                Infoziant DAST Tool
+                            </div>
+                            <div className="you-tube-video">
+                                    <div className="video-thumbnail" onClick={handlePlayVideo}>
+                                        <img
+                                            src={videoimage}
+                                            alt="Infoziant DAST Tool"
+                                            className="thumbnail-image"
+                                        />
+                                        <button className="play-button">
+                                            <svg width="80" height="80" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="50" cy="50" r="48" fill="url(#grad1)" style={{ stroke: 'none' }} />
+                                                <polygon points="40,30 70,50 40,70" fill="white" />
+                                                <defs>
+                                                    <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
+                                                        <stop offset="0%" style={{ stopColor: '#15F5BA', stopOpacity: 1 }} />
+                                                        <stop offset="100%" style={{ stopColor: '#0066ff', stopOpacity: 1 }} />
+                                                    </linearGradient>
+                                                </defs>
+                                            </svg>
+                                        </button>
+                                    </div>
+                            </div>
+                            {isVideoPlaying && (
+                                    <div className="video-container">
+                                        <iframe
+                                            src="https://www.youtube.com/embed/xK-QUTobeeg?autoplay=1"
+                                            title="Infoziant DAST Tool"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowFullScreen
+                                        ></iframe>
+                                        <button className="close-button" onClick={handleCloseVideo}>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <defs>
+                                                    <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
+                                                        <stop offset="0%" style={{ stopColor: '#15F5BA', stopOpacity: 1 }} />
+                                                        <stop offset="100%" style={{ stopColor: '#0066ff', stopOpacity: 1 }} />
+                                                    </linearGradient>
+                                                </defs>
+                                                <text x="12" y="12" fontSize="24" fontWeight="bolder" fill="url(#grad1)" textAnchor="middle" alignmentBaseline="middle">✕</text>
+                                            </svg>
+                                        </button>
+
+                                    </div>
+                                )}
+                        </section>
+
+                        
+
+                <section className="section third-section">
                     <h1>Infoziant DAST Features</h1>
                     <div className="wrapper">
                         <div className="cols">
@@ -154,44 +216,8 @@ const Product = () => {
                 </section>
 
 
-                <section className="section third-section">
-                    <h1>Differences Between Infoziant DAST Pro and Free Versions</h1>
-                </section>
-
                 <section class="section fourth-section">
-                    <h1>Infoziant’s free version has garnered over 70,000 downloads worldwide.</h1>
-                    <div class="download-container">
-                        <div>
-                            <div class="download-box">
-                                <h2>Windows</h2>
-                                <div class="download-info">
-                                    <span>Size (176 MB)</span>
-                                    <span>Current Version (1.0.0)</span>
-                                    <button class="download-button">Download</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="requirements-box">
-                            <h2>System Requirements</h2>
-                            <ul>
-                                <li ><b className='bb'>Operating Systems:</b>
-                                    <ul>
-                                        <li>Windows (64bit)</li>
-                                        <li>Linux all distribution (64bit)</li>
-                                    </ul>
-                                    <p>Note: Linux version is only available with the Pro plan.</p>
-                                </li>
-                                <li><b className='bb'>RAM Requirements:</b>
-                                    <ul>
-                                        <li>Minimum 4GB of RAM for scanning simple websites.</li>
-                                        <li>16GB of RAM is necessary for scanning medium-sized websites.</li>
-                                        <li>For larger websites, 32GB of RAM is required.</li>
-                                    </ul>
-                                </li>
-                                <li><b className='bb'>Storage: 10GB of available disk space</b></li>
-                            </ul>
-                        </div>
-                    </div>
+                   <h1>empty</h1>
                 </section>
 
 
@@ -237,9 +263,45 @@ const Product = () => {
                 </section>
 
                 <section className="section sixth-section">
+  <h1>Infoziant’s free version has garnered over 70,000 downloads worldwide.</h1>
+  <div className="download-container">
+    <div className="download-box">
+      <div className="windows-icon">
+        <i className="fab fa-windows"></i> {/* Windows icon */}
+      </div>
+      <h2>Windows</h2>
+      <div className="download-info">
+        <span><i className="fas fa-file-download"></i> Size (176 MB)</span>
+        <span><i className="fas fa-code-branch"></i> Current Version (1.0.0)</span>
+        <button className="download-button">
+          <i className="fas fa-download"></i> Download {/* Download icon */}
+        </button>
+      </div>
+    </div>
 
-                    <h1>Demo</h1>
-                </section>
+    <div className="requirements-box">
+      <h2><i className="fas fa-tools"></i> System Requirements</h2>
+      <ul>
+        <li><b className='bb'>Operating Systems:</b>
+          <ul>
+            <li><i className="fab fa-windows"></i> Windows (64bit)</li>
+            <li><i className="fab fa-linux"></i> Linux all distribution (64bit)</li>
+          </ul>
+          <p>Note: Linux version is only available with the Pro plan.</p>
+        </li>
+        <li><b className='bb'>RAM Requirements:</b>
+          <ul>
+            <li><i className="fas fa-memory"></i> Minimum 4GB for scanning simple websites.</li>
+            <li><i className="fas fa-memory"></i> 16GB for medium-sized websites.</li>
+            <li><i className="fas fa-memory"></i> 32GB for larger websites.</li>
+          </ul>
+        </li>
+        <li><b className='bb'><i className="fas fa-hdd"></i> Storage:</b> 10GB of available disk space</li>
+      </ul>
+    </div>
+  </div>
+</section>
+
             </div>
         </>
     );
